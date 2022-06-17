c63fb11ede5bf50cc3d9fe1a4ff44f82c308d796
vaso84 <94482467+vaso84@users.noreply.github.com>
2022-06-17 17:05:55 -0400

    Change report format from Excel to CSV

--- patrol/checks/check_instance.py
+++ patrol/checks/check_instance.py
@@ -41,21 +41,32 @@ class CheckInstance(object):
                 df.head(10).to_string(index=False))
 
         # Save report to Excel file
+        # report_dir = '{}/{}'.format(conf.get('core', 'REPORTS_FOLDER'), strftime('%Y-%m-%d'))
+        # report_file = '/{}__{}__{}.xlsx'.format(check.check_id, strftime('%H%M%S'), self.guid)
+        # report_file = report_dir + report_file
+
+        # if not os.path.exists(report_dir):
+        #     os.makedirs(report_dir)
+        
+        # log.info("Saving detailed report to Excel file: %s", report_file)
+
+        # # Configure proper column width in Excel depending on actual values
+        # writer = pd.ExcelWriter(report_file) 
+        # df.to_excel(writer, sheet_name='Report', index=False)  # may consider na_rep='NaN'
+
+        # for column in df:
+        #     column_length = max(df[column].astype(str).map(len).max(), len(column))
+        #     col_idx = df.columns.get_loc(column)
+        #     writer.sheets['Report'].set_column(col_idx, col_idx, column_length)
+        # writer.save()
+
+        # Save report to CSV file
         report_dir = '{}/{}'.format(conf.get('core', 'REPORTS_FOLDER'), strftime('%Y-%m-%d'))
-        report_file = '/{}__{}__{}.xlsx'.format(check.check_id, strftime('%H%M%S'), self.guid)
+        report_file = '/{}__{}__{}.csv'.format(check.check_id, strftime('%H%M%S'), self.guid)
         report_file = report_dir + report_file
 
         if not os.path.exists(report_dir):
             os.makedirs(report_dir)
         
         log.info("Saving detailed report to Excel file: %s", report_file)
-
-        # Configure proper column width in Excel depending on actual values
-        writer = pd.ExcelWriter(report_file) 
-        df.to_excel(writer, sheet_name='Report', index=False)  # may consider na_rep='NaN'
-
-        for column in df:
-            column_length = max(df[column].astype(str).map(len).max(), len(column))
-            col_idx = df.columns.get_loc(column)
-            writer.sheets['Report'].set_column(col_idx, col_idx, column_length)
-        writer.save()
+        df.to_csv(report_file, sep = '\t', index=False)
