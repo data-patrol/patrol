76410ac469e48c9e5a37885b46bccdf193f80d50
Kanan Mammadli <kanan.mammadli@vonage.com>
2022-07-05 19:37:53 -0400

    Reading checks from json file

--- patrol/checks/check_registry.py
+++ patrol/checks/check_registry.py
@@ -1,18 +1,30 @@
 import logging
 import os
 import sys
+import json
+import copy
 
 from patrol import checks
 from patrol.conf import conf
 from patrol.data_model import (session, DQ_Check, DQ_Check_Run)
+from patrol.connectors.connector_factory import ConnectorFactory
+from patrol import class_lib
 
 log = logging.getLogger(__name__)
+
+CHECKS_DIR = conf.get('core', 'CHECKS_FOLDER')
+INHERITED_PARAMS = ['schedule_interval', 'expiry_period', 'rows to persist', 'recipient_list']
+coonection_map = {'my_conn_1': class_lib.Connection(
+                                                'my_conn_1', 
+                                                'Sqlite',
+                                                'consumerdb.db')}
 
 class CheckRegistry(object):
     """
     CheckRegistry contains information about all the DQ checks in the system
     """
     
+
     def __init__(self):
         self.checks = {}
         self.discover_checks()
@@ -26,11 +38,65 @@ class CheckRegistry(object):
         
         session.commit()
 
+    def import_params(self, dic_a, dic_b, keys):
+        """
+        updates dic_a with values from dic_b for keys
+        if value is a list then value in dic_a is extended
+        else it is replaced
+        """
+        for k in keys:
+            if k in dic_b:
+                if isinstance(dic_b[k], list) and (k in dic_a):
+                    dic_a[k].extend(dic_b[k])
+                else:
+                    dic_a[k] = dic_b[k]
+
+    def get_checks(self, tree, params={}, arr=[]):
+        # inherit parameters from upstream
+        self.import_params(params, tree, INHERITED_PARAMS)
+        log.debug(f' ---- {tree.get("name","none")}, params = {params}')
+        
+        # register check elements
+        if 'checks' in tree:
+            for chk in tree['checks']:
+                chk_params = copy.deepcopy(params)
+                cfg_file = open(os.path.join( CHECKS_DIR,chk))
+                cfg_check = json.load(cfg_file)
+                self.import_params(chk_params, cfg_check, INHERITED_PARAMS)
+
+                log.info(f'check_id = {cfg_check["check_id"]}, chk_params = {chk_params}')
+                simpleCheck = checks.SimpleCheck(check_id=cfg_check['check_id'],
+                                                name=cfg_check['name'],
+                                                description=cfg_check['description'],
+                                                schedule_interval=chk_params['schedule_interval'],
+                                                notification = {'expiry_period': chk_params['expiry_period'],
+                                                                'rows to persist': chk_params['rows to persist'],
+                                                                'recipient_list': chk_params['recipient_list']
+                                                                }
+                                                )
+                for step in cfg_check['steps']:
+                    log.info(f'step is {step["step_type"]}')
+                    simpleCheck.add_step(checks.SimpleCheckStep(step_seq = step['step_seq'],
+                                                                step_type = step['step_type'],
+                                                                query = step['query'],
+                                                                connection = coonection_map[step['connection']]))
+                arr.append(simpleCheck)
+                cfg_file.close()
+        
+        # Recursive processing
+        if 'items' in tree:
+            for item in tree['items']:
+                self.get_checks(item, params, arr)
+        return arr
+
     def discover_checks(self):
-        checks_dir = conf.get('core', 'CHECKS_FOLDER')
-        for root, dirs, files in os.walk(checks_dir):
-            for file in files:
-                pass  #TODO
+        log.debug('++++++++++++++++ discover_checks started')
+        with open(os.path.join( CHECKS_DIR,'dq_checks.json')) as f:
+            check_tree = json.load(f)
+            check_list = self.get_checks(check_tree)
+            for ch in check_list:
+                self.add_check(ch)
+        log.debug('++++++++++++++++ discover_checks finished')
 
         
 
--- patrol/data_model.py
+++ patrol/data_model.py
@@ -1,4 +1,5 @@
 import logging
+import json
 
 from sqlalchemy import (
         Column, Integer, String, DateTime, Text, Boolean, ForeignKey)
@@ -13,7 +14,7 @@ from patrol.conf import conf
 
 PATROL_CONN = conf.get('core', 'patrol_conn')
 
-engine = create_engine(PATROL_CONN, echo=True)
+engine = create_engine(PATROL_CONN, echo=False)
 Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 session = Session()
 
@@ -30,6 +31,9 @@ class DQ_Check(Base):
         schedule_interval = Column('schedule_interval', String(16))
         name = Column('name', String(256))
         description = Column('description', String(8192))
+        expiry_period = Column('expiry_period', String(16))
+        persist_rows = Column('persist_rows', Integer())
+        recipient_list = Column('recipient_list', String(1024))
         next_run = Column('next_run', DateTime)
 
         def __init__(self, check):
@@ -37,7 +41,11 @@ class DQ_Check(Base):
                         , check_id = check.check_id
                         , schedule_interval = check.schedule_interval
                         , name = check.name
-                        , description = check.description)   
+                        , description = check.description
+                        , expiry_period = check.notification['expiry_period'] if check.notification else None
+                        , persist_rows = check.notification['rows to persist'] if check.notification else None
+                        , recipient_list = json.dumps(check.notification['recipient_list'] if check.notification else [])
+                        )   
 
         def __repr__(self):
                 return f"<QD_Check(check_id='{self.check_id}', name='{self.name}', schedule_interval='{self.schedule_interval}' )>"
@@ -47,7 +55,7 @@ class DQ_Check_Run(Base):
 
         guid = Column('guid', String(ID_LEN), primary_key = True)
         check_id = Column('check_id', String(ID_LEN), primary_key = True)
-        step_id = Column('step_id', String(ID_LEN), primary_key = True)
+        step_seq = Column('step_seq', Integer, primary_key = True)
         start_time = Column('start_time', DateTime)
         end_time = Column('end_time', DateTime)
         status = Column('status', String(16))
@@ -59,16 +67,12 @@ class DQ_Check_Run(Base):
              Base.__init__(self
                         , guid = check_step.guid
                         , check_id = check_step.check_id
-                        , step_id = check_step.step_id
+                        , step_seq = check_step.step_seq
                         , status = 'INITIAL')   
 
         def __repr__(self):
-                return f"<QD_Check_Run(check_id='{self.check_id}', step_id='{self.step_id}', start_time='{self.start_time}', status='{self.status}' )>"
+                return f"<QD_Check_Run(check_id='{self.check_id}', step_seq='{self.step_seq}', start_time='{self.start_time}', status='{self.status}' )>"
 
 def initdb(args): #TODO
-
-        # dq_check = DQ_Check()
-        # dq_check_run = DQ_Check_Run()
-        # log.info(DQ_Check.__table__)
 
         Base.metadata.create_all(engine)
