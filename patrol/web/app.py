f7627bc17adf86732c0960842de660f7e1fdad27
vaso84 <94482467+vaso84@users.noreply.github.com>
2022-07-18 22:37:22 -0400

    UI detailed report

--- patrol/web/app.py
+++ patrol/web/app.py
@@ -1,5 +1,6 @@
 import logging
 
+from datetime import datetime
 from flask import Flask, render_template
 
 import patrol.data_model as dm
@@ -8,16 +9,21 @@ import patrol.data_model as dm
 app = Flask(__name__)
 
 @app.route("/")
-@app.route('/dq-checks/')
-def dq_checks():
-    return render_template('dq-checks.html')
+@app.route('/checks/')
+def checks():
+    return render_template('checks.html')
 
 @app.route('/connections/')
 def connections():
     return render_template('connections.html')
 
-@app.route('/rep-dq/')
-def rep_dq():
+@app.route('/rpt-detailed/')
+def rpt_detailed():
     dq_check_runs = dm.session.query(dm.DQ_Check_Run).order_by(
-            dm.DQ_Check_Run.start_time.desc()).limit(500)
-    return render_template('rep-dq.html', dq_check_runs=dq_check_runs)
+            dm.DQ_Check_Run.start_time.desc()).limit(5000)
+    return render_template('rpt-detailed.html', datetime=datetime, dq_check_runs=dq_check_runs)
+
+@app.route('/check-details/')
+def check_details():
+    return render_template('check-details.html')
+
