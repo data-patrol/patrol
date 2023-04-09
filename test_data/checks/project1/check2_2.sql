b01d18eed98746815debc1f6f8288571b5ddd0be
Alex Chekulaev <chekulaevalexey@gmail.com>
2023-04-09 13:46:49 -0400

    Update README.md and add test_data

---
+++ test_data/checks/project1/check2_2.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CASE
+        WHEN row_count = 0 THEN 0
+        ELSE 2
+    END as result, 
+    *
+FROM
+(
+    SELECT 
+        COUNT(*) OVER () as row_count,
+        * 
+    FROM orders ord
+    LEFT JOIN customers cus ON ord.customer_id = cus.customer_id
+    WHERE cus.customer_id IS NULL
+)
