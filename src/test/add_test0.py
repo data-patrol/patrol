3e6745ce77f005722fdf71a2d50d5b301477be49
Alex Chekulaev <chekulaevalexey@gmail.com>
2023-04-10 11:58:29 -0400

    move away from pybuilder

--- src/test/add_test0.py
+++
@@ -1,8 +0,0 @@
-# from unittest import TestCase
-#
-# from add import add
-#
-#
-# class AddTest(TestCase):
-#     def test_add(self):
-#         assert add(1, 2) == 3
--- tests/add_test.py
+++ tests/add_test.py
@@ -1,8 +1,12 @@
 from unittest import TestCase
 
 from add import add
+from sub import sub
 
 
 class AddTest(TestCase):
     def test_add(self):
         assert add(1, 2) == 3
+
+    def test_sub(self):
+        assert sub(3, 2) == 1
